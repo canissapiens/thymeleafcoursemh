934584cb37deb6f42eb58071ef9b4e1420c29415
Miroslaw Hardek <miroslaw.hardek@gmail.com>
2019-09-18 12:08:23 +0200

    EOD 17 IX 2019 A

--- src/main/java/pl/mirek/thymeleafcoursemh/dto/CarDto.java
+++ src/main/java/pl/mirek/thymeleafcoursemh/dto/CarDto.java
@@ -6,9 +6,20 @@ public class CarDto {
     public String mark;
     public String model;
 
+    public CarDto() {
+    }
+
     public CarDto(int id, String mark, String model) {
         this.id = id;
         this.mark = mark;
         this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
